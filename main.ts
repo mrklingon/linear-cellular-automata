cdff1894a47c36e59e7f8da9fda0b3605ea44908
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-10-27 20:21:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,74 @@
+function doGen () {
+    for (let index = 0; index <= 4; index++) {
+        doNeighbors(index)
+    }
+    state = result
+}
+input.onButtonPressed(Button.A, function () {
+    doGen()
+    plot_line()
+})
+function doNeighbors (num: number) {
+    tot = 0
+    tot += state[num]
+    if (num == 0) {
+        tot += state[4]
+    } else {
+        tot += state[num - 1]
+    }
+    if (num == 4) {
+        tot += state[0]
+    } else {
+        tot += state[num + 1]
+    }
+    if (1 == tot) {
+        result[num] = 1
+    } else {
+        result[num] = 0
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 5; index++) {
+        doGen()
+        plot_line()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index <= 4; index++) {
+        for (let in2 = 0; in2 <= 4; in2++) {
+            led.unplot(in2, index)
+        }
+    }
+    reset()
+    plot_line()
+})
+function plot_line () {
+    for (let index = 0; index <= 4; index++) {
+        led.unplot(index, line)
+    }
+    for (let index = 0; index <= 4; index++) {
+        if (1 == state[index]) {
+            led.plot(index, line)
+        }
+    }
+    line += 1
+    if (line > 4) {
+        line = 0
+    }
+}
 function reset () {
     state = []
     for (let index = 0; index < 5; index++) {
-        state.insertAt(0, Math.randomBoolean())
+        state.insertAt(0, randint(0, 1))
     }
 }
-let state: boolean[] = []
-state = [false, false, false, false, false]
-let result = [false, false, false, false, false]
+let tot = 0
 let line = 0
-let lca = images.createImage(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
+let result: number[] = []
+let state: number[] = []
+led.unplot(2, 2)
+state = [0, 0, 0, 0, 0]
+result = [0, 0, 0, 0, 0]
 reset()
-turtle.setPosition(0, 0)
+line = 0
+plot_line()
